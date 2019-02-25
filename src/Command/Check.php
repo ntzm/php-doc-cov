9b90708e94329f5d803bde5d8db7c4a2811cac0f
Nat Zimmermann <nat@ntzm.io>
2019-02-25 22:18:07 +0000

    add command, allow reference of classes not in directory

---
+++ src/Command/Check.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocCov\Command;
+
+use Composer\Autoload\ClassLoader;
+use DocCov\CodeRunner;
+use DocCov\CodeTransformer;
+use DocCov\Coverage\Driver;
+use DocCov\Coverage\PcovDriver;
+use DocCov\Coverage\XdebugDriver;
+use DocCov\Extractor\MarkdownExtractor;
+use DocCov\FunctionFinder;
+use Roave\BetterReflection\Reflection\ReflectionMethod;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface as Input;
+use Symfony\Component\Console\Output\OutputInterface as Output;
+
+final class Check extends Command
+{
+    /** @var ClassLoader */
+    private $classLoader;
+
+    public function __construct(ClassLoader $classLoader)
+    {
+        parent::__construct('check');
+
+        $this->classLoader = $classLoader;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('paths', InputArgument::IS_ARRAY | InputArgument::REQUIRED);
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $md = '
+
+```php
+$transformer = new DocCov\CodeTransformer();
+
+$transformer->transform("foo");
+```
+';
+
+        $extractor = new MarkdownExtractor();
+        $transformer = new CodeTransformer();
+        $coverageDriver = $this->getCoverageDriver();
+        $runner = new CodeRunner($coverageDriver);
+
+        foreach ($extractor->extract($md) as $code) {
+            $code = $transformer->transform($code);
+
+            $runner->run($code);
+        }
+
+        $coverage = $coverageDriver->collect();
+
+        $finder = new FunctionFinder($this->classLoader);
+
+        $totalMethods = 0;
+        $totalCoveredMethods = 0;
+
+        /** @var string[] $paths */
+        $paths = $input->getArgument('paths');
+
+        /** @var ReflectionMethod $method */
+        foreach ($finder->find($paths) as $method) {
+            ++$totalMethods;
+
+            $isCovered = $coverage->hasCoveredMethod($method);
+
+            if ($isCovered) {
+                ++$totalCoveredMethods;
+            }
+
+            $output->writeln(($isCovered ? '✔ ' : '✖ ') . $method->getDeclaringClass()->getName() . '::' . $method->getName() . ($isCovered ? ' is covered' : ' is not covered'));
+        }
+
+        if ($totalMethods === 0) {
+            $percentageCovered = '0.00%';
+        } else {
+            $percentageCovered = number_format($totalCoveredMethods / $totalMethods * 100, 2);
+        }
+
+        $output->writeln('Total coverage: ' . $percentageCovered . '%');
+    }
+
+    private function getCoverageDriver(): Driver
+    {
+        if (extension_loaded('pcov') && ini_get('pcov.enabled')) {
+            return new PcovDriver();
+        }
+
+        if (extension_loaded('xdebug')) {
+            return new XdebugDriver();
+        }
+
+        throw new RuntimeException('No coverage driver available');
+    }
+}
--- src/FunctionFinder.php
+++ src/FunctionFinder.php
@@ -4,15 +4,31 @@ declare(strict_types=1);
 
 namespace DocCov;
 
+use Composer\Autoload\ClassLoader;
 use Generator;
 use ReflectionMethod;
 use Roave\BetterReflection\BetterReflection;
 use Roave\BetterReflection\Reflection\ReflectionClass;
 use Roave\BetterReflection\Reflector\ClassReflector;
+use Roave\BetterReflection\SourceLocator\Type\AggregateSourceLocator;
+use Roave\BetterReflection\SourceLocator\Type\ComposerSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\DirectoriesSourceLocator;
 
 final class FunctionFinder
 {
+    private const METHOD_NAMES_BLACKLIST = [
+        '__construct' => true,
+        '__destruct' => true,
+    ];
+
+    /** @var ClassLoader */
+    private $classLoader;
+
+    public function __construct(ClassLoader $classLoader)
+    {
+        $this->classLoader = $classLoader;
+    }
+
     /**
      * @param string[] $paths
      * @return Generator<\Roave\BetterReflection\Reflection\ReflectionMethod>
@@ -20,7 +36,13 @@ final class FunctionFinder
     public function find(array $paths): Generator
     {
         $astLocator = (new BetterReflection())->astLocator();
-        $reflector = new ClassReflector(new DirectoriesSourceLocator($paths, $astLocator));
+
+        $locator = new AggregateSourceLocator([
+            new DirectoriesSourceLocator($paths, $astLocator),
+            new ComposerSourceLocator($this->classLoader, $astLocator),
+        ]);
+
+        $reflector = new ClassReflector($locator);
 
         foreach ($reflector->getAllClasses() as $class) {
             if ($class->isInterface()) {
@@ -32,7 +54,7 @@ final class FunctionFinder
             }
 
             foreach ($class->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-                if ($method->getName() === '__construct') {
+                if (isset(self::METHOD_NAMES_BLACKLIST[$method->getName()])) {
                     continue;
                 }
 
